92f87206afdf4d170c19bb9a8b389c09d3684154
Shogun <shogun@cowtech.it>
2018-06-18 09:39:59 +0200

    chore: Version 2.2.5.

--- lib/ballast/version.rb
+++ lib/ballast/version.rb
@@ -16,7 +16,7 @@ module Ballast
     MINOR = 2
 
     # The patch version.
-    PATCH = 4
+    PATCH = 5
 
     # The current version of ballast.
     STRING = [MAJOR, MINOR, PATCH].compact.join(".")
